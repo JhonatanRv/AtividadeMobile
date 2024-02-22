356dbd4dd9cc560aab8cc4dc0755b612926137c2
jhonatan.0109.vieira <jhonatan.0109.vieira@gmail.com>
2024-02-22 07:41:45 +0000

    Atividade

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="login-form-wrap">
+
+          <form className='login-form'>
+            <img src='https://mentorprofissional.com.br/wp-content/uploads/2018/12/senai-1.png' width="400" height="100"></img>
+    
+            <input type='email' name='email' placeholder='Email' required/>
+            <input type='password' name='password' placeholder='Senha' required/>
+
+            <button type='submit' className='btn-login'>Entrar</button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
